e6386261ce07047c2584808a3712e369830879e7
VICIOne <bavicton@gmail.com>
2021-07-10 17:11:12 +0300

    Added basic routing

--- src/shared/components/SideBar/SideBarListItem/index.jsx
+++ src/shared/components/SideBar/SideBarListItem/index.jsx
@@ -1,31 +1,11 @@
-/* eslint-disable dot-notation */
 import React from 'react';
-// import cx from 'classnames';
-
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-// import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import {NavLink} from 'react-router-dom';
 
 import s from './style.module.scss';
 
-const SitebarListItem = ({text, index, selectedItem, handleListItemClick}) => (
-  <ListItem
-    className={s.listItem}
-    key={`${text}_${index}`}
-    classes={{
-      selected: s.selectedItem,
-    }}
-    selected={selectedItem === `${text}_${index}`}
-    onClick={() => handleListItemClick(`${text}_${index}`)}
-  >
-    <ListItemText primary={text} className={s['listItem__text']} />
-    {/* ????? */}
-    {/* <ArrowForwardIcon
-      className={cx(
-        s['listItem__arrowIcon'],
-        selectedItem === `${text}_${index}` ? s['listItem__arrowIcon__active'] : ''
-      )}
-    /> */}
-  </ListItem>
+const SitebarListItem = ({name, path}) => (
+  <NavLink to={path || '/without'} className={s.listItem} activeClassName={s.listItem__selectedItem}>
+    {name || 'WITHOUT'}
+  </NavLink>
 );
 export default SitebarListItem;
